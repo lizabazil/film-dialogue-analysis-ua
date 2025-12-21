958117abb8fe83610df3ffa6f2397a7b27b52a62
Yelyzaveta Tretiak <liza.tretjak@gmail.com>
2025-12-21 20:14:33 +0100

    init draft for gender detection module

--- src/modules/speaker_enrichment/gender_identifier.py
+++ src/modules/speaker_enrichment/gender_identifier.py
@@ -1,5 +1,25 @@
 # this file will be using all methods for identifying the gender of the speaker
+from audio_gender_extractor import AudioGenderExtractor
+from visual_gender_extractor import VisualGenderExtractor
+from text_gender_extractor import TextGenderExtractor
+
+
 class GenderEnricher:
+    """
+    Main class which uses different approaches to detect the gender of the speaker.
+    """
     # TODO: implement
-    def __init__(self):
+    def __init__(self, config: dict, video_path: str):
+        self.weights = {
+            "audio": config.get("speaker_enrichment", {}).get("voting_weights", {}).get("audio", {}),
+            "image": config.get("speaker_enrichment", {}).get("voting_weights", {}).get("image", {}),
+            "text": config.get("speaker_enrichment", {}).get("voting_weights", {}).get("text", {})
+        }
+
+        self.audio_gender_extractor = AudioGenderExtractor(config, video_path)
+        self.visual_gender_extractor = VisualGenderExtractor(config, video_path)
+        self.text_gender_extractor = TextGenderExtractor()
+
+    def annotate_segments(self, segments: list):
+        # TODO: implement
         pass
--- src/modules/speaker_enrichment/text_gender_extractor.py
+++ src/modules/speaker_enrichment/text_gender_extractor.py
@@ -1,1 +1,10 @@
 # TODO: implement
+from basic_gender_extractor import BasicGenderExtractor
+
+
+class TextGenderExtractor(BasicGenderExtractor):
+    def __init__(self):
+        super().__init__()
+
+    def predict_gender(self, segments: list) -> dict | None:
+        pass
