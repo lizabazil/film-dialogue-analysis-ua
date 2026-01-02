49f827e88f82c023471ae140466458cf85df988d
Yelyzaveta Tretiak <liza.tretjak@gmail.com>
2026-01-02 16:59:08 +0100

    init version of llm_merger; not checked yet

--- src/modules/transcription/llm_transcript_merger.py
+++ src/modules/transcription/llm_transcript_merger.py
@@ -39,8 +39,8 @@ class LLMTranscriptMerger:
     def __init__(self, chunk_length_in_min: int, overlap_in_min: float):
         self.chunk_length_in_min = chunk_length_in_min
         self.overlap_in_min = overlap_in_min
-        self.step_ms = TimeUtils.convert_to_ms(TimeUtils.convert_to_ms(0, self.chunk_length_in_min, 0, 0) -
-                                               TimeUtils.convert_to_ms(0, self.overlap_in_min, 0, 0))
+        self.step_ms = (TimeUtils.convert_to_ms(0, self.chunk_length_in_min, 0, 0) -
+                        TimeUtils.convert_to_ms(0, self.overlap_in_min, 0, 0))
 
     def _set_to_proper_timecode(self, chunk_num: int, timecode: TimeCode) -> TimeCode:
         """
@@ -79,7 +79,7 @@ class LLMTranscriptMerger:
         Writes new transcript to another file with proper timecodes
         """
         txt_file_path = os.path.abspath(txt_file_path)
-        speaker_id, speech, chunk_id = None, None, 0
+        #speaker_id, speech, chunk_id = None, None, 0
         FileUtils.delete_file(txt_output_file_path)
 
         with open(txt_file_path, "r") as raw_file:
@@ -91,39 +91,40 @@ class LLMTranscriptMerger:
                 chunk = "\n".join(group)
                 all_chunks.append(chunk)
 
-        for line in lines:
-            print(f"\nLINE:\n{line}")
+        # for line in lines:
+        #     print(f"\nLINE:\n{line}")
+        #
+        #     # get the chunk number if it is line with this information
+        #     if line.startswith("-- CHUNK "):
+        #         chunk_id = int(re.findall(r"\d+", line)[0])
+        #         continue
+        #
+        #     timecodes = re.findall(r"\[(.*?)]", line)[0]  # get everything between '[' and ']'
+        #     timecode_start, timecode_end = timecodes.split(" - ")
+        #     timecode_start = self._parse_timecode(timecode_start)
+        #     timecode_end = self._parse_timecode(timecode_end)
+        #
+        #     # set to proper timecode (according to the whole movie)
+        #     timecode_start_h, timecode_start_m, timecode_start_s, timecode_start_ms = self._set_to_proper_timecode(
+        #         chunk_id,
+        #         timecode_start)
+        #
+        #     timecode_end_h, timecode_end_m, timecode_end_s, timecode_end_ms = self._set_to_proper_timecode(chunk_id,
+        #                                                                                                    timecode_end)
+        #
+        #     speaker_id = _get_speaker(line)
+        #     speech = _get_speech(line)
+        #
+        #     output_line = (
+        #         f"{speaker_id} | {timecode_start_h}:{timecode_start_m}:{timecode_start_s}.{timecode_start_ms}"
+        #         f" --> {timecode_end_h}:{timecode_end_m}:{timecode_end_s}.{timecode_end_ms} | "
+        #         f"{speech}")
 
-            # get the chunk number if it is line with this information
-            if line.startswith("-- CHUNK "):
-                chunk_id = int(re.findall(r"\d+", line)[0])
-                continue
-
-            timecodes = re.findall(r"\[(.*?)]", line)[0]  # get everything between '[' and ']'
-            timecode_start, timecode_end = timecodes.split(" - ")
-            timecode_start = self._parse_timecode(timecode_start)
-            timecode_end = self._parse_timecode(timecode_end)
-
-            # set to proper timecode (according to the whole movie)
-            timecode_start_h, timecode_start_m, timecode_start_s, timecode_start_ms = self._set_to_proper_timecode(
-                chunk_id,
-                timecode_start)
-
-            timecode_end_h, timecode_end_m, timecode_end_s, timecode_end_ms = self._set_to_proper_timecode(chunk_id,
-                                                                                                           timecode_end)
-
-            speaker_id = _get_speaker(line)
-            speech = _get_speech(line)
-
-            output_line = (
-                f"{speaker_id} | {timecode_start_h}:{timecode_start_m}:{timecode_start_s}.{timecode_start_ms}"
-                f" --> {timecode_end_h}:{timecode_end_m}:{timecode_end_s}.{timecode_end_ms} | "
-                f"{speech}")
-
-            # write (append) to a new file
-            with open(txt_output_file_path, 'a') as output_file:
-                output_file.write(output_line)
-                output_file.write("\n")
+        final_lines = self._remove_expressive_overlap_speech(all_chunks)
+        # write (append) to a new file
+        with open(txt_output_file_path, 'a') as output_file:
+            for line in final_lines:
+                output_file.write(f"{line}\n")
 
     def _remove_expressive_overlap_speech(self, raw_chunks: list[str]) -> list[str]:
         final_lines = []
@@ -156,7 +157,7 @@ class LLMTranscriptMerger:
                 end_time_formatted = _format_time_str(*end_time_tuple)
 
                 new_timecodes = (f"[{start_time_formatted} --> "
-                          f"{end_time_formatted}]")
+                                 f"{end_time_formatted}]")
 
                 speaker = _get_speaker(line)
                 speech = _get_speech(line)
