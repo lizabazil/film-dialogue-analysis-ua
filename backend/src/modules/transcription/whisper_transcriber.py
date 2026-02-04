6660c44f5196718587baf87f19214e76b5d47b3b
Yelyzaveta Tretiak <liza.tretjak@gmail.com>
2026-02-04 11:16:52 +0100

    reorganised files

--- backend/src/modules/transcription/whisper_transcriber.py
+++ backend/src/modules/transcription/whisper_transcriber.py
@@ -1,8 +1,10 @@
 """
 File for transcribing audio using the OpenAI Whisper model.
 """
+import numpy as np
 import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from typing import Union
 
 
 class WhisperTranscriber:
@@ -15,7 +17,8 @@ class WhisperTranscriber:
             config (dict): Dictionary with configuration data.
         """
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
-        self.model_id = config.get("speech_to_text_model", {}).get("whisper_model_name", "openai/whisper-large-v3")
+        #self.model_id = config.get("speech_to_text_model", {}).get("whisper_model_name", "openai/whisper-large-v3")
+        self.model_id = "openai/whisper-large-v3-turbo" #"openai/whisper-base" #
         self.torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
         self.model = AutoModelForSpeechSeq2Seq.from_pretrained(
             self.model_id, torch_dtype=self.torch_dtype, low_cpu_mem_usage=True,
@@ -24,17 +27,18 @@ class WhisperTranscriber:
 
         self.model.to(self.device)
 
-        processor = AutoProcessor.from_pretrained(self.model_id)
+        self.processor = AutoProcessor.from_pretrained(self.model_id)
 
         self.pipe = pipeline(
             "automatic-speech-recognition",
             model=self.model,
-            tokenizer=processor.tokenizer,
-            feature_extractor=processor.feature_extractor,
-            torch_dtype=self.torch_dtype,
+            tokenizer=self.processor.tokenizer,
+            feature_extractor=self.processor.feature_extractor,
+            dtype=self.torch_dtype,
             device=self.device,
-            return_timestamps=True,
+            #return_timestamps=True,
         )
+        print(f"Whisper init done for {self.model_id}")
 
     def get_whisper_transcription(self, audio_path: str) -> str:
         """
@@ -54,3 +58,22 @@ class WhisperTranscriber:
 
         transcription = result['text']
         return transcription
+
+    def get_whisper_transcription_from_array(self, audio_input: Union[str, np.ndarray], sample_rate: int = 16000) -> str:
+        """
+        Get text from given audio input (file path or raw numpy array).
+        """
+        if isinstance(audio_input, np.ndarray):
+            prediction_input = {"raw": audio_input, "sampling_rate": sample_rate}
+        else:
+            prediction_input = audio_input
+
+        result = self.pipe(
+            prediction_input,
+            generate_kwargs={
+                "task": "transcribe",
+                "language": "ukrainian",
+            },
+        )
+
+        return result['text']
