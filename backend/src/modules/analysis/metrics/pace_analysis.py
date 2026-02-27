525df3bc184eab4a9844b9cca18a9908d03addb2
Yelyzaveta Tretiak <liza.tretjak@gmail.com>
2026-02-27 12:14:16 +0100

    draft for pace analysis

---
+++ backend/src/modules/analysis/metrics/pace_analysis.py
@@ -0,0 +1,141 @@
+from collections import Counter
+
+from .base import BaseMetric
+from src.utils.segment import Segment
+from src.modules.analysis.schemas import (SegmentPaceMetrics, TurnTransition, PaceTrendPoint, PaceGlobalAnalysis,
+                                          ReplicaType, PauseType)
+
+
+class PaceAnalysis(BaseMetric):
+    def calculate(self, segments: list[Segment], **kwargs) -> PaceGlobalAnalysis:
+        all_replicas_type = [self._classify_replica(s).category for s in segments]
+        # total_short_replicas = sum(1 for replica in all_replicas_type if replica == ReplicaType.REACTIVE)
+        # total_standard_replicas = sum(1 for r in all_replicas_type if r == ReplicaType.STANDARD)
+        # total_extended_replicas = sum(1 for r in all_replicas_type if r == ReplicaType.EXTENDED)
+        total_monologue_replicas = sum(1 for r in all_replicas_type if r == ReplicaType.MONOLOGUE)
+
+        all_transitions = self._analyze_all_transitions(segments)
+        all_transitions_type = [pause.pause_category for pause in all_transitions]
+        total_long_pauses = sum(1 for p in all_transitions_type if p == PauseType.LONG_PAUSE)
+        total_small_pauses = sum(1 for p in all_transitions_type if p == PauseType.SMALL)
+
+        pace_trends = self._generate_pace_trends(segments)
+        return PaceGlobalAnalysis(
+            total_monologues=total_monologue_replicas,
+            total_long_pauses=total_long_pauses,
+            total_instant_responses=total_small_pauses,
+            pace_graph=pace_trends
+        )
+        pass
+
+    def _classify_replica(self, segment: Segment) -> SegmentPaceMetrics:
+        total_words = 0
+        if segment.nlp_data:
+            total_words = sum(
+                1
+                for sentence in segment.nlp_data
+                for token in sentence
+                if token.get("upos") != "PUNCT"
+            )
+
+        duration_seconds = (segment.total_ms_end - segment.total_ms_start) / 1000
+        if total_words <= 3:
+            replica_type = ReplicaType.REACTIVE
+        elif total_words <= 15:
+            replica_type = ReplicaType.STANDARD
+        elif total_words <= 40:
+            replica_type = ReplicaType.EXTENDED
+        else:
+            replica_type = ReplicaType.MONOLOGUE
+
+        return SegmentPaceMetrics(word_count=total_words,
+                                  duration_sec=duration_seconds,
+                                  category=replica_type)
+
+    def _calculate_transition(self, prev_segment: Segment, curr_segment: Segment) -> TurnTransition:
+        pause_ms = (curr_segment.total_ms_start - prev_segment.total_ms_end)
+        if pause_ms <= 200:
+            pause_type = PauseType.SMALL
+        elif pause_ms <= 1000:
+            pause_type = PauseType.NORMAL_PAUSE
+        elif pause_ms <= 2000:
+            pause_type = PauseType.HESITATION
+        else:
+            pause_type = PauseType.LONG_PAUSE
+        return TurnTransition(duration_seconds=pause_ms / 1000,
+                              pause_category=pause_type)
+
+    def _analyze_all_transitions(self, segments: list[Segment]) -> list[TurnTransition]:
+        transitions = []
+        for i in range(1, len(segments)):
+            prev = segments[i - 1]
+            curr = segments[i]
+            transition = self._calculate_transition(prev, curr)
+            transitions.append(transition)
+
+        return transitions
+
+    def _generate_pace_trends(self, segments: list[Segment], window_seconds: int = 60, step_seconds: int = 30) -> list[PaceTrendPoint]:
+        total_movie_duration_ms = segments[-1].total_ms_end
+        window_ms = window_seconds * 1000
+        step_ms = step_seconds * 1000
+
+        all_transitions = self._analyze_all_transitions(segments)  # pauses
+
+        current_window_end_time_ms = window_ms
+        trends = []
+        while current_window_end_time_ms <= total_movie_duration_ms:
+            # get all segments in the current window
+            window_start_ms = current_window_end_time_ms - window_ms
+            window_segments = [s for s in segments if s.total_ms_start >= window_start_ms and s.total_ms_end <= current_window_end_time_ms]
+
+            # calculate pace trend point for this window
+            curr_pace_trend_point = self._calculate_pace_trend_point_for_window(window_segments, segments,
+                                                                                all_transitions)
+            trends.append(curr_pace_trend_point)
+
+            current_window_end_time_ms += step_ms
+
+        return trends
+
+    def _calculate_pace_trend_point_for_window(self, window_segments: list[Segment], segments: list[Segment],
+                                               all_transitions: list[TurnTransition]) -> (PaceTrendPoint | None):
+        if not window_segments:
+            return None
+
+        window_ms_start = window_segments[0].total_ms_start
+        window_duration_seconds = window_segments[-1].total_ms_end - window_ms_start
+
+        classified_replicas = [self._classify_replica(s) for s in window_segments]  # segment pace metrics object
+        total_words_in_window = sum(replica.word_count for replica in classified_replicas)
+
+        first_seg_index = segments.index(window_segments[0])
+        last_seg_index = segments.index(window_segments[-1])
+
+        window_transitions = all_transitions[first_seg_index:last_seg_index]  # those transitions in the given window
+
+        all_replicas_types = [segment.category for segment in classified_replicas]
+        dominant_replica_type, _ = Counter(all_replicas_types).most_common(1)[0]
+
+        all_pauses_types = [p.category.pause_category for p in window_transitions]
+        dominant_pause_type, _ = Counter(all_pauses_types).most_common(1)[0]
+
+        total_silence_seconds = sum(transition.duration_seconds for transition in window_transitions)
+        silence_percentage = (total_silence_seconds / window_duration_seconds) * 100
+
+        return PaceTrendPoint(timestamps_total_ms=window_ms_start,
+                              total_local_words=total_words_in_window,
+                              silence_percentage=silence_percentage,
+                              dominant_replica_type=dominant_replica_type,
+                              dominant_pause_type=dominant_pause_type)
+
+    def _evaluate_overall_pace(self, trends: list[PaceTrendPoint]) -> str:
+        """
+        Дивиться на середній activity_score по всьому фільму та повертає текстову мітку («Динамічний», «Повільний»).
+        Args:
+            trends:
+
+        Returns:
+
+        """
+        pass
--- backend/src/modules/analysis/schemas.py
+++ backend/src/modules/analysis/schemas.py
@@ -1,4 +1,54 @@
 from pydantic import BaseModel
+from enum import Enum
+
+
+class ReplicaType(str, Enum):  # about replica type (how short/long it is)
+    REACTIVE = "reactive"  # very short replicas (1-3 words only)
+    STANDARD = "standard"  # normal replicas (4-15 words)
+    EXTENDED = "extended"  # extended replicas (16-40)
+    MONOLOGUE = "monologue"   # long monologues  (>= 40 words or >= 30 seconds long)  # TODO: probably delete about monologue duration
+
+
+class PauseType(str, Enum):   # what is happening between replicas
+    SMALL = "small"  # very quick reaction to the prev segment (0-200 ms)
+    NORMAL_PAUSE = "normal_pause"  # max 1 second
+    HESITATION = "hesitation"  # 1-2 seconds
+    LONG_PAUSE = "long_pause"  # > 2 seconds
+
+
+class SegmentPaceMetrics(BaseModel):  # info about segment type
+    word_count: int
+    duration_sec: float
+    category: ReplicaType
+
+
+class TurnTransition(BaseModel):  # about pause between two consequent segments
+    duration_seconds: float
+    pause_category: PauseType
+
+
+class PaceTrendPoint(BaseModel):  # for one sliding window
+    timestamps_total_ms: float  # ms after the movie's start
+    total_local_words: int
+    silence_percentage: float  # percentage of silence in this window  (0-100)
+
+    dominant_replica_type: ReplicaType  # which type of replicas was the most common in this window
+    dominant_pause_type: TurnTransition
+
+
+class PaceGlobalAnalysis(BaseModel):
+    #result_pace_label: str  # slow, normal, dynamic
+
+    total_monologues: int
+    total_long_pauses: int
+    total_instant_responses: int
+
+    pace_graph: list[PaceTrendPoint]  # list of all windows
+
+
+class BechdelTest(BaseModel):
+    passed_bechdel_test: bool
+    passed_points: int  # how many points were passed for this test (1, 2, or 3)
 
 
 class GenderBalance(BaseModel):
@@ -7,9 +57,9 @@ class GenderBalance(BaseModel):
 
     woman_replicas: int
     man_replicas: int
-    avg_words_per_replica_woman: float   # mlu
+    avg_words_per_replica_woman: float
     avg_words_per_replica_man: float
-    bechdel_test: bool  # whether dialogues has passed the Bechdel test
+    bechdel_test: BechdelTest  # info about Bechdel test
     #man_time_ms: int
     #woman_time_ms: int
     #man_word_count: int
